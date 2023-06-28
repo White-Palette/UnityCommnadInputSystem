15d58499f6ed027f58d961436a7a67fb8161c28e
hun1001 <l01022899266@gmail.com>
2023-06-28 18:35:15 +0900

    제대로 작동하는지 확인 할 수 있는 UI 제작중

---
+++ Assets/Scripts/ArrowImageDirection.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowImageDirection : MonoBehaviour
+{
+    private Direction _direction = Direction.Neutral;
+
+    public void SetDirection(Direction direction)
+    {
+        this._direction = direction;
+
+        switch (_direction)
+        {
+            case Direction.LeftDown:
+                transform.rotation = Quaternion.Euler(0, 0, 225);
+                break;
+            case Direction.Down:
+                transform.rotation = Quaternion.Euler(0, 0, 270);
+                break;
+            case Direction.RightDown:
+                transform.rotation = Quaternion.Euler(0, 0, 315);
+                break;
+            case Direction.Left:
+                transform.rotation = Quaternion.Euler(0, 0, 180);
+                break;
+            case Direction.Right:
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            case Direction.LeftUp:
+                transform.rotation = Quaternion.Euler(0, 0, 135);
+                break;
+            case Direction.Up:
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+                break;
+            case Direction.RightUp:
+                transform.rotation = Quaternion.Euler(0, 0, 45);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,13 @@
+
+public enum Direction
+{
+    LeftDown = 1,
+    Down = 2,
+    RightDown = 3,
+    Left = 4,
+    Neutral = 5,
+    Right = 6,
+    LeftUp = 7,
+    Up = 8,
+    RightUp = 9
+}
