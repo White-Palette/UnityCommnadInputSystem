96c03b73df10c1453bbeaf8a930e1a853c52df3b
hun1001 <l01022899266@gmail.com>
2023-07-08 18:16:33 +0900

    한글 되네. Input VirtualStick.cs 완료함. UI임

--- Assets/Scripts/UI/VirtualJoyStick/VirtualStick.cs
+++ Assets/Scripts/UI/VirtualJoyStick/VirtualStick.cs
@@ -10,16 +10,25 @@ public class VirtualStick : MonoBehaviour
 
     private void Awake()
     {
-        _stick = transform.Find("Stick").GetComponent<RectTransform>();
+        _stick = transform.GetChild(0).Find("Stick").GetComponent<RectTransform>();
     }
 
     private void Start()
     {
-        InputEventManager.AddEvent(Direction.LeftDown, () => { });
+        InputEventManager.AddEvent(Direction.LeftDown, () => MoveStick(-_moveDistance, -_moveDistance) );
+        InputEventManager.AddEvent(Direction.Down, () => MoveStick(0f, -_moveDistance) );
+        InputEventManager.AddEvent(Direction.RightDown, () => MoveStick(_moveDistance, -_moveDistance) );
+        InputEventManager.AddEvent(Direction.Left, () => MoveStick(-_moveDistance, 0f) );
+        InputEventManager.AddEvent(Direction.Right, () => MoveStick(_moveDistance, 0f) );
+        InputEventManager.AddEvent(Direction.LeftUp, () => MoveStick(-_moveDistance, _moveDistance) );
+        InputEventManager.AddEvent(Direction.Up, () => MoveStick(0f, _moveDistance) );
+        InputEventManager.AddEvent(Direction.RightUp, () => MoveStick(_moveDistance, _moveDistance) );
+        
+        InputEventManager.AddEvent(Direction.Neutral, () => MoveStick(0f, 0f) );
     }
 
     public void MoveStick(float posX, float posY)
     {
-
+        _stick.localPosition = new Vector3(posX, posY, 0f);
     }
 }
